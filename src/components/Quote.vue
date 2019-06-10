fdc8f093288255cf32efbd76915d1121a4ff84ec
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-06-10 10:06:48 -0300

    Add files via upload

---
+++ src/components/Quote.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="panel panel-default">
+            <div class="panel-body quote">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['quotes'],
+    
+    created(){
+        console.log('Quote');
+    }
+}
+</script>
+
+<style>
+    .panel-body {
+        font-family: 'Arizonia', cursive;
+        font-size: 24px;
+        color: #6e6e6e
+    }
+
+    .quote {
+        cursor: pointer;
+    }
+
+    .quote:houver {
+        background-color: #ffe2e2;
+    }
+</style>
